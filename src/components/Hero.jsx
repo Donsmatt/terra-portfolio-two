0ca70860dbaa9f7bd7ba007deb5e9cc7478156fb
Adachukwu24 <adachukwuezinwa@gmail.com>
2025-07-02 19:44:39 +0100

    created hero and navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='hero-div'>
+        <h4>🟢Available for freelancing </h4>
+        <h2>Designing with <span>passion</span>, creativity, and a touch of magic.</h2>
+        <h3>Need top-notch design services? I’ve got you covered!</h3>
+        <button>👋 Hire me</button>
+    </div>
+  )
+}
