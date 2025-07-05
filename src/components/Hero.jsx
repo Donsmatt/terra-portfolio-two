eeb2a9c0003ac5f7e988d22d54cfb61e1b52a082
Collins <donsmatt44@gmail.com>
2025-07-05 10:12:39 +0100

    hero section fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-
 export default function Hero() {
   return (
-    <div className='hero-div'>
-        <h4>🟢Available for freelancing </h4>
-        <h2>Designing with <span>passion</span>, creativity, and a touch of magic.</h2>
-        <h3>Need top-notch design services? I’ve got you covered!</h3>
-        <button>👋 Hire me</button>
-    </div>
-  )
+    <section className="hero__section">
+      <div className="container hero__section-wrapper">
+        <span className="hero__section-span">🟢Available for freelancing </span>
+        <h2 className="hero__section-title">
+          Designing with <span className="accent">passion</span>, creativity, and a touch of magic.
+        </h2>
+        <p className="hero__section-text">
+          Need top-notch design services? I’ve got you covered!
+        </p>
+        <div className=".btn-container">
+          <button className="hero__section-button">👋 Hire me</button>
+        </div>
+      </div>
+    </section>
+  );
 }
