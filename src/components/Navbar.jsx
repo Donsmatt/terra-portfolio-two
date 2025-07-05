3323e84f829c73420a55f48c775fc5b477ca9c15
Collins Adaigho <106975277+Donsmatt@users.noreply.github.com>
2025-07-05 12:45:52 +0100

    Smatt (#5)
    * about section and slider section done
    
    * added a container style
    
    * navbar fixed
    
    * hero section fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-
 export default function Navbar() {
   return (
     <nav>
-        <div className="left">
-            <p>By Jesse</p>Product Designer
+      <div className="navbar container">
+        <h1 className="navbar__logo">
+          <span className="ff-accent">By Jesse</span> Product Designer
+        </h1>
+        <div className="navbar__items">
+          <ul className="navbar__list">
+            <li>Work</li>
+            <li>About Me</li>
+            <li>Skills</li>
+          </ul>
+          <div className="btn-container">
+            <button className="navbar__btn">Contact</button>
+          </div>
         </div>
-        <div className="right">
-            <ul>
-                <li>Work</li>
-                <li>About Me</li>
-                <li>Skills</li>
-                <li>Contact</li>
-            </ul>
-        </div>
+      </div>
     </nav>
-  )
+  );
 }
